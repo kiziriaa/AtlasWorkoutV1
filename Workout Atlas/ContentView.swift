8550d4cfdc6dd38b5e855c6a5b1ade3a73af491d
kiziriaa <102491494+kiziriaa@users.noreply.github.com>
2023-07-15 15:18:10 -0600

    Initial Commit

---
+++ Workout Atlas/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Workout Atlas
+//
+//  Created by Alexandre Kizirian on 7/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Workout Atlas/Workout_AtlasApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_AtlasApp.swift
+//  Workout Atlas
+//
+//  Created by Alexandre Kizirian on 7/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Workout_AtlasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
